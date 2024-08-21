abd262268433eacaff6a8f0b3587628ab1dcf5d3
Gabriel <juanmendez0013@gmail.com>
2024-08-21 00:11:08 -0300

    creamos el client gateway

---
+++ src/products/products.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+@Controller('products')
+export class ProductsController {
+  constructor() {}
+
+  @Post()
+  createProduct() {
+    return 'Crea un producto';
+  }
+
+  @Get()
+  findAllProducts() {
+    return 'Esta funcion regresa varios productos';
+  }
+
+  @Get(':id')
+  find(@Param('id') id: string) {
+    return `Esta funcion regresa el producto ${+id}`;
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return `Esta funcion elimina el producto ${+id}`;
+  }
+
+  @Patch(':id')
+  patchProduct(@Param('id') id: string, @Body() body: any) {
+    return `Esta funcion actualiza el producto ${+id}`;
+  }
+}
