91f6d9bf71314a896db0a4004057b1ae4eca948e
Gabriel <juanmendez0013@gmail.com>
2024-08-26 21:11:28 -0300

    se cambio de TCP a NATS

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -11,7 +11,7 @@ import {
   Query,
 } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
-import { PRODUCT_SERVICE } from 'src/config';
+import { NATS_SERVICE } from 'src/config';
 import { PaginationDto } from '../common/dto/pagination.dto';
 import { catchError, firstValueFrom } from 'rxjs';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -19,24 +19,16 @@ import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
-  constructor(
-    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
-  ) {}
+  constructor(@Inject(NATS_SERVICE) private readonly natsClient: ClientProxy) {}
 
   @Post()
   createProduct(@Body() createProductDto: CreateProductDto) {
-    return this.productsClient.send(
-      { cmd: 'create_product' },
-      createProductDto,
-    );
+    return this.natsClient.send({ cmd: 'create_product' }, createProductDto);
   }
 
   @Get()
   findAllProducts(@Query() paginationDto: PaginationDto) {
-    return this.productsClient.send(
-      { cmd: 'find_all_products' },
-      paginationDto,
-    );
+    return this.natsClient.send({ cmd: 'find_all_products' }, paginationDto);
   }
 
   @Get(':id')
@@ -49,7 +41,7 @@ export class ProductsController {
      */
     try {
       const product = await firstValueFrom(
-        this.productsClient.send({ cmd: 'find_one_product' }, { id }).pipe(
+        this.natsClient.send({ cmd: 'find_one_product' }, { id }).pipe(
           catchError((err) => {
             throw new RpcException(err);
           }),
@@ -64,7 +56,7 @@ export class ProductsController {
 
   @Delete(':id')
   deleteProduct(@Param('id') id: string) {
-    return this.productsClient.send({ cmd: 'delete_product' }, { id });
+    return this.natsClient.send({ cmd: 'delete_product' }, { id });
   }
 
   @Patch(':id')
@@ -72,7 +64,7 @@ export class ProductsController {
     @Param('id', ParseIntPipe) id: number,
     @Body() updateProductDto: UpdateProductDto,
   ) {
-    return this.productsClient
+    return this.natsClient
       .send(
         { cmd: 'updated_product' },
         {
