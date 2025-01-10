d9129c740ff404ae96992e2fd313fd85385f4f9d
Gabriel <juanmendez0013@gmail.com>
2025-01-09 21:44:12 -0300

    se agrega módulo de autenticación con controladores y DTOs

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { NATS_SERVICE } from 'src/config';
+import { LoginUserDto, RegisterUserDto } from './dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(@Inject(NATS_SERVICE) private readonly natsClient: ClientProxy) {}
+
+  @Post('register')
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.natsClient.send('auth.register.user', registerUserDto);
+  }
+
+  @Post('login')
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.natsClient.send('auth.login.user', loginUserDto);
+  }
+
+  @Get('verify')
+  verifyToken() {
+    return this.natsClient.send('auth.verify.user', {});
+  }
+}
